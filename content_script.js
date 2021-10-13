5ec64e9df03ee7615357dad72e2810e74ea73070
Pedro Flores <pflores@dncp.gov.py>
2021-10-13 11:47:49 -0300

    dropped innerHTMl usage for innerText

--- content_script.js
+++ content_script.js
@@ -1,11 +1,11 @@
 console.log('itavy-o started...');
 
 let tavyshos = document.querySelectorAll('.numeros');
-const korekooo = () => tavyshos.forEach(t => t.innerHTML = '*');
-const waaa = () => tavyshos.forEach(t => t.innerHTML = t.tavyoVal);
+const korekooo = () => tavyshos.forEach(t => t.innerText = '*');
+const waaa = () => tavyshos.forEach(t => t.innerText = t.tavyoVal);
 
 tavyshos.forEach(t => { 
-  t.tavyoVal = t.innerHTML; 
+  t.tavyoVal = t.innerText; 
   t.addEventListener('mouseover', () => korekooo()); 
   t.addEventListener('mouseout', () => waaa());
 });
